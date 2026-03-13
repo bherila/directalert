1d05147e79a25b768d49265f517861583347d169
Ben Herila <ben+gh@herila.net>
2026-03-13 00:31:18 -0700

    feat: integrate Brevo mailer transport

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'brevo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,6 +44,10 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
+        ],
+
+        'brevo' => [
+            'transport' => 'brevo',
         ],
 
         'ses' => [
